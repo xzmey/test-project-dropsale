6130bc49e36136d075aaf17a9481897ce0eb6ea8
a.shaidullin <OpusRickWiseAddress>
2023-10-13 16:00:17 +0400

    replica

---
+++ .docker/slave/slave.sql
@@ -0,0 +1,1 @@
+CHANGE MASTER TO MASTER_HOST='test-project-mysql-master', MASTER_USER='repl', MASTER_PASSWORD='slavepass';
--- database/migrations/2023_10_12_204216_create_users_table.php
+++ database/migrations/2023_10_12_204216_create_users_table.php
@@ -11,15 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::connection("mysql::read")->create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('first_name')->index();
-            $table->string('last_name')->index();
-            $table->string('email')->unique();
-            $table->smallInteger('age');
-        });
-
-        Schema::connection("mysql::write")->create('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('first_name')->index();
             $table->string('last_name')->index();
@@ -33,7 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::connection("mysql.read")->dropIfExists('users');
-        Schema::connection("mysql.write")->dropIfExists('users');
+        Schema::dropIfExists('users');
     }
 };
